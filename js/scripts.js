70d3aed17620030201bf6256d6d0d788b349a447
Katherine Matthews <katherinematthews.polsci@gmail.com>
2017-01-27 10:22:01 -0800

    add event listener for click on pizza size. when size is clicked on, it is stored into a variable and a new object is made with user's toppings and size preferences.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
-function Order(pizzaToppings) {
+function Order(pizzaToppings, pizzaSize) {
   this.toppings = pizzaToppings;
+  this.size = pizzaSize;
+}
+
+Order.prototype.price = function() {
+  
 }
 
 var toppings = [];
+var size = "";
 
 $(document).ready(function() {
   $(".topping").click(function() {
     var topping = this.textContent;
     toppings.push(topping);
     $("#results").empty().text(toppings);
-    var user = new Order(toppings);
+  });
+  $(".size").click(function() {
+    var diameter = this.textContent;
+    size = diameter;
+    var user = new Order(toppings, size);
     console.log(user);
   });
 });
